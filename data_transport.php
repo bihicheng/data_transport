8fa7abfdb777c34025047a529e6bb5f7760ad486
bihicheng <hichengbi@mugeda.com>
2015-10-22 14:17:24 +0800

    add log

--- data_transport.php
+++ data_transport.php
@@ -1,6 +1,6 @@
 <?php
-
 // author: hichengbi@mugeda.com
+
 require_once 'operation.php';
 require_once 'mongodb_tool.php';
 
@@ -9,7 +9,8 @@ $constants = Constants::getContants();
 $mongo_tool = MongodbTool::getInstance();
 $user_operation = $mongo_tool->get_user_operations();
 
-$seconds = intval($argv[1]);
+
+$seconds = isset($argv[1]) ? intval($argv[1]) : 0;
 if($seconds <= 0) $seconds = 10;
 
 while(true) {
@@ -19,7 +20,13 @@ while(true) {
 		echo OperationLogger::count($key) . PHP_EOL;
 		while(OperationLogger::count($key) > 0) {
 			$val = OperationLogger::pop_record($key);
-			$user_operation->insert(json_decode($val));	
+			try {
+				$user_operation->insert(json_decode($val));	
+			} catch (Exception $e) {
+				file_put_contents('error.log', $e->getMessage() . PHP_EOL, FILE_APPEND);
+				file_put_contents('transport.log', $val . PHP_EOL , FILE_APPEND);
+			}
+			
 		}
 	}
 	OperationLogger::quit(); // close redis connection
